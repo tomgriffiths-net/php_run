75103436227378ae51aab2b851f7fa9668ed2aab
Tom <tomgriffiths2007@gmail.com>
2026-01-15 08:44:34 +0000

    v2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # php_run
 This is a package for PHP-CLI that allows php code to be run as a command.
+
+# Usage
+php_run [-bs, -json, -timer, -noend] <php code>
+
+# Options
+- **bs**: Escapes backslashes in the command, e.g. so you dont have to type \\\\ in file paths.
+- **json**: Returns the return of the ran code as json to the console.
+- **timer**: Adds a timer and says how long it took to run the code.
+- **noend**: Stops the default behaviour of adding a semicolon at the end of the command.
--- main.php
+++ main.php
@@ -1,7 +1,53 @@
 <?php
 class php_run{
     public static function command($line):void{
-        $line = str_replace("\\","\\\\",$line);
-        eval($line);
+        trim($line);
+
+        $options = [];
+
+        while(substr($line, 0, 1) === "-"){
+            $nextSpace = strpos($line, " ", 1);
+            $options[] = strtolower(substr($line, 1, $nextSpace -1));
+            $line = substr($line, $nextSpace +1);
+        }
+
+        $giveJson = false;
+        $timer = false;
+        $addSemicolon = true;
+
+        foreach($options as $option){
+            if($option === "bs"){
+                $line = str_replace("\\","\\\\",$line);
+            }
+            elseif($option === "json"){
+                $giveJson = true;
+            }
+            elseif($option === "timer"){
+                $timer = true;
+            }
+            elseif($option === "noend"){
+                $addSemicolon = false;
+            }
+        }
+
+        if($addSemicolon){
+            $line = $line . ";";
+        }
+
+        if($timer){
+            $timer = microtime(true);
+        }
+
+        if($giveJson){
+            echo "Return: " . json_encode(eval("return " . $line), JSON_PRETTY_PRINT) . "\n";
+        }
+        else{
+            eval($line);
+        }
+
+        if(is_float($timer)){
+            $timer = microtime(true) - $timer;
+            echo "Time taken: " . round($timer, 6) . "s\n";
+        }
     }
 }
